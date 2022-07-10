b934c6ef54e6a6d972a562c7d01a27581dc0b32a
Ashwanth18 <Ashwanth18@gmail.com>
2022-07-11 02:56:15 +0800

    adding useState page 4 lol

--- registration_form/pages/page4.jsx
+++ registration_form/pages/page4.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import {useForm, Controller, SubmitHandler} from "react-hook-form";
 import {yupResolver} from "@hookform/resolvers/yup";
 import * as yup from "yup";
