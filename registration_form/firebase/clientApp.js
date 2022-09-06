645c0fb19022f3020e85221d1458e69d1a9400df
Ashwanth18 <Ashwanth18@gmail.com>
2022-09-06 21:42:36 +0800

    redeploying for the environment variables

--- registration_form/firebase/clientApp.js
+++ registration_form/firebase/clientApp.js
@@ -4,7 +4,7 @@ import { getFirestore } from 'firebase/firestore'
 // import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-export {db};
+ export default {app};
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
