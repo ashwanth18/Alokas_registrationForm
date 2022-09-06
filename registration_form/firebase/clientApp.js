31563da0f00672b49c5b1258c7264c6d6a635816
Ashwanth18 <Ashwanth18@gmail.com>
2022-09-06 21:57:21 +0800

    fixed export issues in firebase clientApp.js

--- registration_form/firebase/clientApp.js
+++ registration_form/firebase/clientApp.js
@@ -4,7 +4,6 @@ import { getFirestore } from 'firebase/firestore'
 // import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
- export default {app};
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -21,3 +20,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 // const analytics = getAnalytics(app);
+
+export default app;
